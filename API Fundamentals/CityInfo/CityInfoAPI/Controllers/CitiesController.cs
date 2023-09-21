d7a5b74ac5e7aa21731e3288cb629d66a023c42f
Kishore <kishore1207india@gmail.com>
2023-09-21 23:21:55 +0530

    sample action method created

---
+++ API Fundamentals/CityInfo/CityInfoAPI/Controllers/CitiesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CitiesController : ControllerBase
+    {
+        public JsonResult GetCitites()
+        {
+            return new JsonResult(
+                new List< object >{
+                    new { Id = 1,Name="Chennai" },
+                    new {Id = 2,Name="Mumbai" }
+            });
+        }
+    }
+}
