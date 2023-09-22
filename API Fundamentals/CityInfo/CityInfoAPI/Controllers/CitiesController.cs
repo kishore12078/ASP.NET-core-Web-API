4a5ae573c200c7db081405ccf97e2a620ed92ad8
Kishore <kishore1207india@gmail.com>
2023-09-22 20:31:33 +0530

    learning attribute based routing

--- API Fundamentals/CityInfo/CityInfoAPI/Controllers/CitiesController.cs
+++ API Fundamentals/CityInfo/CityInfoAPI/Controllers/CitiesController.cs
@@ -3,17 +3,27 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CityInfoAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/cities")]
     [ApiController]
     public class CitiesController : ControllerBase
     {
+        [HttpGet]
         public JsonResult GetCitites()
         {
-            return new JsonResult(
-                new List< object >{
-                    new { Id = 1,Name="Chennai" },
-                    new {Id = 2,Name="Mumbai" }
-            });
+            //return new JsonResult(
+            //    new List< object >{
+            //        new { Id = 1,Name="Chennai" },
+            //        new {Id = 2,Name="Mumbai" }
+            //});
+            CityDataStore cityDataStore = new CityDataStore();
+            return new JsonResult(cityDataStore.Cities);
+            //return new JsonResult(CityDataStore.Current.Cities);
+        }
+
+        [HttpGet("{id}")]
+        public JsonResult GetCityById(int id)
+        {
+            return new JsonResult(CityDataStore.Current.Cities.FirstOrDefault(c => c.Id == id));
         }
     }
 }
