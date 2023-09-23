118d90e4565255efcab30d8034c03c86a6cd128f
Kishore <kishore1207india@gmail.com>
2023-09-23 13:43:26 +0530

    formatter and negotiation practised

--- API Fundamentals/CityInfo/CityInfoAPI/Program.cs
+++ API Fundamentals/CityInfo/CityInfoAPI/Program.cs
@@ -8,7 +8,9 @@ namespace CityInfoAPI
 
             // Add services to the container.
 
-            builder.Services.AddControllers();
+            builder.Services.AddControllers(options =>
+            options.ReturnHttpNotAcceptable = true
+                ).AddXmlDataContractSerializerFormatters() ;
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
