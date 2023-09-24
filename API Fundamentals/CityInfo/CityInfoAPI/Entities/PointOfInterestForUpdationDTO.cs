679ab439719183c80cd5186951c239b516b0e5b8
Kishore <kishore1207india@gmail.com>
2023-09-24 13:42:03 +0530

    validation applied for the models

---
+++ API Fundamentals/CityInfo/CityInfoAPI/Entities/PointOfInterestForUpdationDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CityInfoAPI.Entities
+{
+    public class PointOfInterestForUpdationDTO
+    {
+        [Required(ErrorMessage = "You Should Provide Name of the City")]
+        [MaxLength(20, ErrorMessage = "Name should be Maximum of 20 characters")]
+        public string Name { get; set; } = string.Empty;
+        [MaxLength(200, ErrorMessage = "Description should be maximum of 200 Characters")]
+        public string Description { get; set; } = string.Empty;
+    }
+}
