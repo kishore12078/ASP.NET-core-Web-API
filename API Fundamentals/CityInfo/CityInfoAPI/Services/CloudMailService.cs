639cdfc192c88e565f2e643b09b6c23f9fa2a82d
Kishore <kishore1207india@gmail.com>
2023-09-26 23:30:46 +0530

    Cloud mail services created

---
+++ API Fundamentals/CityInfo/CityInfoAPI/Services/CloudMailService.cs
@@ -0,0 +1,16 @@
+﻿using CityInfoAPI.Interfaces;
+
+namespace CityInfoAPI.Services
+{
+    public class CloudMailService : IMailService
+    {
+        //private variables must start with underscore
+        private string _to { get; set; } = "kishore1207india@gmail.com";
+        private string _from { get; set; } = "kishore1207india@gmail.com";
+
+        public void Send(string subject, string message)
+        {
+            Console.WriteLine($"Mail has been sended to {_to} from {_from}, with {nameof(CloudMailService)}" + "\n" + $"Subject:{subject}" + "\n" + $"Content: {message}");
+        }
+    }
+}
