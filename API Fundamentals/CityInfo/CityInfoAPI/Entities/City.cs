d09e3bbdf11fddf25c9fe00fbf584f4935b80bde
Kishore <kishore1207india@gmail.com>
2023-09-28 22:50:14 +0530

    created city and pointofinterest entity classes with foreign key relation

--- API Fundamentals/CityInfo/CityInfoAPI/Entities/City.cs
+++ API Fundamentals/CityInfo/CityInfoAPI/Entities/City.cs
@@ -2,16 +2,5 @@
 {
     public class City
     {
-        public int Id { get; set; }
-        public string Name { get; set; }=string.Empty;
-        public string Description { get; set; } = string.Empty;
-        public int CityPoints
-        {
-            get
-            {
-                return PointsOfInterests.Count();
-            }
-        }
-        public List<PointsOfInterest> PointsOfInterests { get; set; }=new List<PointsOfInterest>();
     }
 }
