6471c142bfbce5647af242825a08c711285f35f3
Kishore <kishore1207india@gmail.com>
2023-09-30 13:17:09 +0530

    entity class's relation changed

--- API Fundamentals/CityInfo/CityInfoAPI/Entities/City.cs
+++ API Fundamentals/CityInfo/CityInfoAPI/Entities/City.cs
@@ -10,7 +10,6 @@ namespace CityInfoAPI.Entities
             Name = name;
         }
         [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         [Required]
         [MaxLength(20)]
