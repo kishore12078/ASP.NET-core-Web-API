fd533a93582bc2766c3bb32450d46e52e435b8d8
Kishore <kishore1207india@gmail.com>
2023-10-01 17:39:08 +0530

    created new pointOfInterest profile

---
+++ API Fundamentals/CityInfo/CityInfoAPI/Profiles/PointOfInterestProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace CityInfoAPI.Profiles
+{
+    public class PointOfInterestProfile:Profile
+    {
+        public PointOfInterestProfile()
+        {
+            CreateMap<Entities.PointOfInterest, Entities.PointsOfInterestDTO>();
+        }
+    }
+}
