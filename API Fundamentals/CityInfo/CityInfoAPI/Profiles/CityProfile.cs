76447473c5b8c47c3903f8e8c7b01d7948a167d3
Kishore <kishore1207india@gmail.com>
2023-10-01 13:14:20 +0530

    Profile class created for mapping

---
+++ API Fundamentals/CityInfo/CityInfoAPI/Profiles/CityProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace CityInfoAPI.Profiles
+{
+    public class CityProfile:Profile
+    {
+        public CityProfile()
+        {
+            CreateMap<Entities.City, Models.CitiesWithoutPointOfInterestsDTO>();
+        }
+    }
+}
