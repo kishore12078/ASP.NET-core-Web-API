269fd44ea22b1a036b27efe7e5e6e55f698d3326
Kishore <kishore1207india@gmail.com>
2023-10-02 23:21:12 +0530

    Create separate class for Pagenation Metadata to find total pagecount

---
+++ API Fundamentals/CityInfo/CityInfoAPI/Models/PagenationMetadata.cs
@@ -0,0 +1,18 @@
+﻿namespace CityInfoAPI.Models
+{
+    public class PagenationMetadata
+    {
+        public int TotalItemCount { get; set; }
+        public int TotalPageCount { get; set; }
+        public int PageSize { get; set; }
+        public int CurrentPage { get; set; }
+
+        public PagenationMetadata(int totalItemCount,int pageSize,int currentPage)
+        {
+            TotalItemCount = totalItemCount;
+            PageSize= pageSize;
+            CurrentPage = currentPage;
+            TotalPageCount = (int)Math.Ceiling(totalItemCount / (double)pageSize);
+        }
+    }
+}
